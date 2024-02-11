6612c1e0f2b908927c75f3869c38a58c148eef31
Dylan Snel <dylan@diditsolutions.nl>
2024-02-11 15:20:27 +0100

    Add provider concat operator

---
+++ Source/SourceGeneratorsToolkit/Providers/ProviderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace SourceGeneratorsToolkit.Providers;
+
+public static class ProviderExtensions
+{
+    public static IncrementalValuesProvider<TSource> Concat<TSource>(this IncrementalValuesProvider<TSource> source, IncrementalValuesProvider<TSource> other)
+    {
+        return source.Collect().Combine(other.Collect()).SelectMany((x, y) => x.Left.Concat(x.Right));
+    }
+
+}
