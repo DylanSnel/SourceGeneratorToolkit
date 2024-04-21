0b67d0bff20e0a23ad8e592cae2c1341f1c738b9
Dylan Snel <dylan@diditsolutions.nl>
2024-04-21 14:42:43 +0200

    Fixed some propery operations

--- Source/SourceGeneratorsToolkit/SyntaxExtensions/PropertyDeclarationExtensions.cs
+++ Source/SourceGeneratorsToolkit/SyntaxExtensions/PropertyDeclarationExtensions.cs
@@ -6,24 +6,24 @@ using System.Linq;
 namespace SourceGeneratorsToolkit.SyntaxExtensions;
 public static class PropertyDeclarationExtensions
 {
-    public static string GetName(this PropertyDeclarationSyntax properyDeclarationSyntax)
+    public static string GetName(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
-        return properyDeclarationSyntax.Identifier.Text;
+        return propertyDeclarationSyntax.Identifier.Text;
     }
 
-    public static string GetAccessModifier(this PropertyDeclarationSyntax properyDeclarationSyntax)
+    public static string GetAccessModifier(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
-        return properyDeclarationSyntax.Modifiers.FirstOrDefault(m => m.IsKind(SyntaxKind.PublicKeyword) || m.IsKind(SyntaxKind.InternalKeyword) || m.IsKind(SyntaxKind.PrivateKeyword)).Text;
+        return propertyDeclarationSyntax.Modifiers.FirstOrDefault(m => m.IsKind(SyntaxKind.PublicKeyword) || m.IsKind(SyntaxKind.InternalKeyword) || m.IsKind(SyntaxKind.PrivateKeyword) || m.IsKind(SyntaxKind.ProtectedKeyword)).Text;
     }
 
     public static bool HasGetter(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
-        return propertyDeclarationSyntax.AccessorList?.Accessors.Any(x => x.IsKind(SyntaxKind.GetAccessorDeclaration) || x.IsKind(SyntaxKind.InitAccessorDeclaration)) ?? false;
+        return propertyDeclarationSyntax.AccessorList?.Accessors.Any(x => x.IsKind(SyntaxKind.GetAccessorDeclaration)) ?? false;
     }
 
     public static bool HasSetter(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
-        return propertyDeclarationSyntax.AccessorList?.Accessors.Any(x => x.IsKind(SyntaxKind.SetAccessorDeclaration) || x.IsKind(SyntaxKind.InitAccessorDeclaration)) ?? false;
+        return propertyDeclarationSyntax.AccessorList?.Accessors.Any(x => x.IsKind(SyntaxKind.SetAccessorDeclaration)) ?? false;
     }
 
     public static bool HasInitSetter(this PropertyDeclarationSyntax propertyDeclarationSyntax)
@@ -31,25 +31,28 @@ public static class PropertyDeclarationExtensions
         return propertyDeclarationSyntax.AccessorList?.Accessors.Any(x => x.IsKind(SyntaxKind.InitAccessorDeclaration)) ?? false;
     }
 
-    public static bool HasSetSetter(this PropertyDeclarationSyntax propertyDeclarationSyntax)
+    public static bool HasProtectedSetter(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
-        return propertyDeclarationSyntax.AccessorList?.Accessors.Any(x => x.IsKind(SyntaxKind.SetAccessorDeclaration)) ?? false;
+        return propertyDeclarationSyntax.AccessorList?.Accessors.Any(x =>
+            x.IsKind(SyntaxKind.SetAccessorDeclaration) &&
+            x.Modifiers.Any(m => m.IsKind(SyntaxKind.ProtectedKeyword))
+        ) ?? false;
     }
 
     public static string GetGetterModifier(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
-        return propertyDeclarationSyntax.AccessorList?.Accessors.FirstOrDefault(x => x.IsKind(SyntaxKind.GetAccessorDeclaration) || x.IsKind(SyntaxKind.InitAccessorDeclaration))?.Modifiers.FirstOrDefault(m => m.IsKind(SyntaxKind.PublicKeyword) || m.IsKind(SyntaxKind.InternalKeyword) || m.IsKind(SyntaxKind.PrivateKeyword) || m.IsKind(SyntaxKind.ProtectedKeyword)).Text ?? "";
+        return propertyDeclarationSyntax.AccessorList?.Accessors.FirstOrDefault(x => x.IsKind(SyntaxKind.GetAccessorDeclaration))
+            ?.Modifiers.FirstOrDefault(m => m.IsKind(SyntaxKind.PublicKeyword) || m.IsKind(SyntaxKind.InternalKeyword) || m.IsKind(SyntaxKind.PrivateKeyword) || m.IsKind(SyntaxKind.ProtectedKeyword)).Text ?? "";
     }
 
     public static string GetSetterModifier(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
-        return propertyDeclarationSyntax.AccessorList?.Accessors.FirstOrDefault(x => x.IsKind(SyntaxKind.SetAccessorDeclaration) || x.IsKind(SyntaxKind.InitAccessorDeclaration))?.Modifiers.FirstOrDefault(m => m.IsKind(SyntaxKind.PublicKeyword) || m.IsKind(SyntaxKind.InternalKeyword) || m.IsKind(SyntaxKind.PrivateKeyword) || m.IsKind(SyntaxKind.ProtectedKeyword)).Text ?? "";
+        return propertyDeclarationSyntax.AccessorList?.Accessors.FirstOrDefault(x => x.IsKind(SyntaxKind.SetAccessorDeclaration))
+            ?.Modifiers.FirstOrDefault(m => m.IsKind(SyntaxKind.PublicKeyword) || m.IsKind(SyntaxKind.InternalKeyword) || m.IsKind(SyntaxKind.PrivateKeyword) || m.IsKind(SyntaxKind.ProtectedKeyword)).Text ?? "";
     }
 
     public static string GetReturnType(this PropertyDeclarationSyntax propertyDeclarationSyntax)
     {
         return propertyDeclarationSyntax.Type.ToString().Replace("String", "string");
     }
-
-
 }
